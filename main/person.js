42fb1c69cc5684ade1d29a73e7d957dc8b65b1de
Christine233 <1040551136@qq.com>
2017-06-17 21:31:43 +0800

    yzz

--- main/person.js
+++ main/person.js
@@ -1,2 +1,12 @@
 // Write your code here
+class Person {
+	constructor(a,b) {
+	this.myname = a;
+	this.age = b;
+	}
+
+	introduce() {
+		return "My name is " + this.myname + ". " + "I am " + this.age + " years old.";
+	}
+}
 module.exports = Person
--- main/student.js
+++ main/student.js
@@ -1,3 +1,13 @@
 let Person = require('./person')
 // Write your code here
+class Student extends Person {
+	constructor(x,y,klass){
+	super(x,y);
+	this.klass = klass;
+}
+	introduce() {
+		return "My name is "+this.myname+". "+"I am "+this.age+" years old. I am a Student. "+"I am at Class "+this.klass+".";
+}
+}
+
 module.exports = Student
